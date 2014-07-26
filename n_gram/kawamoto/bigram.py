d26b2b8ad04d7919d72cdde707e66f52ab962256
Takashi Kawamoto <kawamoto@yotsuya.tkl.u-tokyo.ac.jp>
2014-07-26 15:37:26 +0900

    clean in this directory

--- n_gram/kawamoto/bigram.py
+++ n_gram/kawamoto/bigram.py
@@ -5,6 +5,8 @@ from sys import *
 from collections import deque
 from collections import defaultdict
 import codecs
+import gzip
+import pickle
 
 #可変長引数
 def ngram(*args, **kwargs):
@@ -19,39 +21,34 @@ def multi_dimension_dict(dimension, callable_obj=int):
 # http://materia.jp/blog/20121119.html
 
 if __name__ == "__main__":
-#    if len(argv) != 2:
-#        print("Usage: # python %s \"N\" " % argv[0])
-#        quit()
+    if len(argv) != 2:
+        print("Usage: # python %s \"TARGET_FILE_PATH\" " % argv[0], file=stderr)
+        quit()
     N = 2
     queue = deque()
     dict = multi_dimension_dict(2)
     i = 0
-    stdin = codecs.getreader('utf8')(stdin.detach(), errors='ignore')
-    for line in stdin:
-        print ("Now line : %d processing!!" % i)
-        i += 1
-        try:
-            word = line.split()[0]
-            if len(queue) == N:
-                if word == "EOS":
-                    dict[queue[0]][queue[1]] += 1
-                    queue.popleft()
-                    queue.append(word)
-                    dict[queue[0]][queue[1]] += 1
-                    queue.clear()
-                else:
-                    dict[queue[0]][queue[1]] += 1
-                    queue.popleft()
-                    queue.append(word)
-            else:
+    print ("Processing : %s" % argv[1], file=stderr)
+    for line in gzip.open(argv[1],'rb'):
+#    for line in gzip.open('./hogehoge.gz','rb'):
+        word = line.split()[0]
+        if len(queue) == N:
+            if word == "EOS":
+                dict[queue[0]][queue[1]] += 1
+                queue.popleft()
                 queue.append(word)
-        except IndexError:
-            print ("IndexError")
-
+                dict[queue[0]][queue[1]] += 1
+                queue.clear()
+            else:
+                dict[queue[0]][queue[1]] += 1
+                queue.popleft()
+                queue.append(word)
+        else:
+            queue.append(word)
+    
 #    dict[queue[0]][queue[1]] += 1
-    print (dict)
+#    print (dict)
     for k1, dict2 in dict.items():
-        print (k1,dict2)
         for k2, v in dict2.items():
-            print (k1,k2,v)
+            print (k1.decode('utf-8'),k2.decode('utf-8'),v)
 
