57b5795b958c12c0f2a8f5284685bc1556c873dd
arieee <ysuzuki@tkl.iis.u-tokyo.ac.jp>
2014-07-31 16:45:17 +0900

    modify correct Laplace smoothing

--- n_gram/ysuzuki/bin/perplexity.py
+++ n_gram/ysuzuki/bin/perplexity.py
@@ -6,14 +6,17 @@ from ngramCrawler import *
 from decimal import *
 
 class Perplexity:
-    def __init__(self,date,n):
+    def __init__(self,testfilename,n):
         # set counts dictionary
         self.n = n
         self.countdic = dict()
         self.sumdic = dict()
+        self.vocabulary_size = 0
+        self.testfilename = testfilename
 
         print "start learning"
-        f = open("../data/"+str(n)+"gram.tsv","r")
+        #f = open("../data/"+str(n)+"gram.tsv","r")
+        f = open("../data/2012-01-01_10","r")
         
         line_num = 0
         for line in f:
@@ -21,62 +24,45 @@ class Perplexity:
             if line_num % 10000 == 0:
                 print "learning line_num :", line_num
 
+            #items = line.strip().split("\t")
+            #ngramtuple = tuple(items[:-1])
+            #counts = items[-1]
             items = line.strip().split("\t")
-            ngramtuple = tuple(items[:-1])
-            counts = items[-1]
+            counts = int(items[0])
+            wordlist = items[1].strip().split(" ") # n=1の時は ["I"]のようにlistになる
 
             #sumdic
-            if n > 1:
-                pre_ngramtuple = tuple(items[:-2])
-                if pre_ngramtuple in self.sumdic:
-                    self.sumdic[pre_ngramtuple]["counts"] += counts
-                    self.sumdic[pre_ngramtuple]["types"] += 1
-                else:
-                    self.sumdic[pre_ngramtuple] = {"counts":counts,"types":1}
+            n_1gramtuple = tuple(wordlist[:-1]) # n=1の時は [] 空リスト→  ) 空tupleに
+            if n_1gramtuple in self.sumdic:
+                self.sumdic[n_1gramtuple]["count"] += counts
+                self.sumdic[n_1gramtuple]["types"} += 1
             else:
-                if self.sumdic:
-                    self.sumdic["counts"] += counts
-                    self.sumdic["types"] += 1
-                else:
-                    self.sumdic = {"counts":counts,"types":1}
+                self.sumdic[n_1gramtuple] = {"counts":counts,"types":1}
 
             #countdic
+            ngramtuple = tuple(wordlist)
             if ngramtuple in self.countdic:
                 self.countdic[ngramtuple] += counts
             else:
                 self.countdic[ngramtuple] = counts
 
-            #unknown words
-            #print "unk check"
-            #for sentence in dataFetcher(date):
-            #    nc = ngramCorpus(sentence)
-            #    ngramlist = nc.ngramMaker(n)
-            #    for ngram in ngramlist:
-            #        if ngram in sumdic:
-            #            pass
-            #        else:
-            #            unkset.add(ngram)
-            #            unkcounts += 1
 
     def laplace_prob(self,ngramtuple):
         sumcounts = 0
         counts = 0
 
         ## calc sumcounts
-        if n > 1:
-            pre_ngramtuple = tuple(ngramtuple[:-1])
-            if pre_ngramtuple in self.sumdic:
-                sumcoutns = self.sumdic[pre_ngramtuple]["counts"] + self.sumdic[pre_ngramtuple]["types"] #laplace_smoothing
-            else:
-                return None # if unknown words exist, skipped
-        else: # n = 1
-            sumcounts = self.sumdic["counts"]+self.sumdic["types"]
+        n_1gramtuple = tuple(ngramtuple[:-1])
+        if n_1gramtuple in self.sumdic:
+            sumcoutns = self.sumdic[n_1gramtuple]["counts"] + self.vocabulary_size + 1 #laplace_smoothing + UNK
+        else:
+            sumcounts = 0 + self.vocabulary_size + 1
 
         ## calc counts
         if ngramtuple in self.sumdic:
             counts = self.countdic[ngramtuple] + 1 #laplace_smoothing
         else:
-            counts = 1
+            counts = 0 + 1
         
         #return Decimal(counts) / Decimal(sumcounts)
         return float(counts) / sumcounts
@@ -85,26 +71,21 @@ class Perplexity:
         print "start calcurating perplexity"
         sum_log_prob = 0
         wordcount = 0
-        sentence_num = 0
-        for sentence in dataFetcher(date):
-            sentence_num += 1
-            if sentence_num % 10000 == 0:
-                print "perplexity sentence_num :", sentence_num
 
+        for sentence in dataFetcher(self.testfilename):
             nc = ngramCorpus(sentence)
             for ngram in nc.ngramMaker(self.n):
                 prob = self.laplace_prob(ngram)
-                if prob:
-                    sum_log_prob += math.log(prob)
-                    wordcount += 1
-                else:
-                    pass
+                sum_log_prob += math.log(prob)
+                wordcount += 1
 
         return math.exp( float((-1)) * sum_log_prob / wordcount) #perplexity
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    date = sys.argv[2]
+    #date = sys.argv[2]
 
-    px = Perplexity(date,n)
+    testfilename = sys.argv[2]
+    #px = Perplexity(date,n)
+    px = Perplexity(testfilename,n)
     print px.perplexity()
