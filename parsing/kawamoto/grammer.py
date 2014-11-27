542a3fb6a127a4a366f06666752e161a1fe46736
Takashi Kawamoto <kawamoto@yotsuya.tkl.u-tokyo.ac.jp>
2014-11-27 18:21:50 +0900

    modify bugs (extra TAGs are dupulicated)

--- parsing/kawamoto/grammer.py
+++ parsing/kawamoto/grammer.py
@@ -20,30 +20,33 @@ class lang:
     def make_cnfs(self):
         self.cnfs_lexicon = self.lexicon
         self.cnfs = []
+        self.temp_gram = []
         for cfg in self.cfgs:
             self.cnfs.extend(self.cfg2cnf(cfg))
+        self.cnfs.extend(self.temp_gram)
     def is_terminal(self, sig):
         return self.lexicon.has_key(sig)
     def cfg2cnf(self,cfg):
         cnfs = []
-        if (len(cfg[1]) > 2):
-            index = 0
-            self.non_terminal.append(len(self.non_terminal))
-            cnfs.append((cfg[0],(cfg[1][index],len(self.non_terminal)-1)))
-            index += 1
-            while(len(cfg[1])-index>2):
+        if (len(cfg[1]) == 3):
+            if( self.search_gram(cfg[1][0],cfg[1][1],self.temp_gram) ==[] ):
                 self.non_terminal.append(len(self.non_terminal))
-                cnfs.append((len(self.non_terminal-1),(cfg[1][index],len(self.non_terminal)-1)))
-                index += 1
-            cnfs.append((len(self.non_terminal)-1,(cfg[1][index],cfg[1][index+1])))
+                self.temp_gram.append( (cfg[0],(len(self.non_terminal),cfg[1][2])) )
+                self.temp_gram.append( (len(self.non_terminal),(cfg[1][0],cfg[1][1])) )
+            else:
+                cnfs.append((cfg[0],(self.search_gram(cfg[1][0],cfg[1][1],self.temp_gram)[0],cfg[1][2])))
         elif(len(cfg[1]) == 2):
             cnfs.append(cfg)
-        else:
+        elif(len(cfg[1]) == 1):
             if self.is_terminal(cfg[1][0]):
                 self.cnfs_lexicon[cfg[0]]=self.cnfs_lexicon[cfg[1][0]]
             for i in self.cfgs:
                 if cfg[1][0]==i[0]:
                     cnfs.extend( self.cfg2cnf((cfg[0],i[1]) ))
+        else:
+            self.non_terminal.append(len(self.non_terminal))
+            cnfs.append( (cfg[0],(len(self.non_terminal),cfg[1][len(cfg[1])-1])) )
+            cnfs.extend( self.cfg2cnf(cfg[1][:-1]) )
         return cnfs
     def taglist(self,word):
         taglist = []
@@ -51,9 +54,11 @@ class lang:
             if y.count(word) > 0:
                 taglist.append(x)
         return tuple(taglist)
-    def search_gram(self,tagA,tagB):
+    def search_gram(self,tagA,tagB, cnfs=None):
         taglist = []
-        for x,y in self.cnfs:
+        if cnfs==None:
+            cnfs = self.cnfs
+        for x,y in cnfs:
             if y==(tagA,tagB):
                 taglist.append(x)
         return taglist
---
+++ parsing/kawamoto/parser.py
@@ -0,0 +1,28 @@
+from grammer import *
+class CKYparser:
+    def __init__(self,words,grammer):
+        self.words = words
+        self.grammer = grammer
+        self.table = [['']*(len(words)+1) for i in xrange((len(words)))]
+    def mktable(self):
+        for i in xrange(len(self.words)):
+            for j in xrange(i+1):
+                if j == 0:
+                    self.table[i][i+1] = self.grammer.taglist(self.words[i])
+                else:
+                    taglist = []
+                    for k in xrange(j):
+                        for tagA in self.table[i-(k+1)][i-j+1]:
+                            for tagB in self.table[i][i-j+k+2]:
+                                taglist.extend(self.grammer.search_gram(tagA,tagB))
+                    self.table[i-j][i+1] = tuple(taglist)
+
+if __name__ == '__main__':
+    L = lang('/home/kawamoto/NLP/parsing/kawamoto/data/lexicon.csv','/home/kawamoto/NLP/parsing/kawamoto/data/grammer.csv')
+    L.make_cnfs()
+    Parser = CKYparser(['book','the','flight','through','Houston'],L)
+    Parser.mktable()
+    for i in Parser.table:
+        for j in i:
+            print j,'\t|',
+        print
