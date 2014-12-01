388c554711b5b4f507b839684a320bd4104d5417
Naoki Yoshinaga <y.naoki@gmail.com>
2014-12-01 18:30:08 +0900

    implement recover_trees

--- parsing/yng/cky.py
+++ parsing/yng/cky.py
@@ -9,21 +9,19 @@ rs2l = collections.defaultdict (list)
 
 def cky_parse (sentence):
     n = len (sentence)
-    cky_table = [[set () for j in range (n + 1)] for i in range (n + 1)]
+    cky_table = [[collections.defaultdict (list) for j in range (n + 1)]
+                 for i in range (n + 1)]
     # lookup lexicon
     for i in range (n):
         for lhs in rs2l[tuple (["_" + sentence[i]])]:
-            cky_table[i][i + 1].add (lhs)
-        if len (cky_table[i][i + 1]) == 0:
-            sys.stderr.write ("unknown word: %s\n" % sentence[i])
-            return cky_table
+            cky_table[i][i + 1][lhs].append (sentence[i])
     # parse
     for i in range (2, n + 1):
         for j in reversed (range (i + 1)):
             for k in range (j, i):
                 for rhs in itertools.product (cky_table[j][k], cky_table[k][i]):
                     for lhs in rs2l[rhs]:
-                        cky_table[j][i].add (lhs)
+                        cky_table[j][i][lhs].append ([[j,k,i], rhs])
     return cky_table
 
 def num_edges (cky_table):
@@ -31,6 +29,23 @@ def num_edges (cky_table):
                 for i in range (len (cky_table))
                 for j in range (i + 1))
 
+def num_trees (cky_table, label = 'S', beg = 0, end = 0):
+    if end - beg == 1: # terminal
+        return 1 if label in cky_table[beg][end] else 0
+    return sum (num_trees (cky_table, x, j, k) *
+                num_trees (cky_table, y, k, i)
+                for (j, k, i), (x, y) in cky_table[beg][end][label])
+
+def recover_trees (cky_table, label = 'S', beg = 0, end = 0):
+    if end - beg == 1: # terminal
+        yield [label, cky_table[beg][end][label][0]]
+    else:
+        for (j, k, i), (x, y) in cky_table[beg][end][label]:
+            for l, r in itertools.product (recover_trees (cky_table, x, j, k),
+                                           recover_trees (cky_table, y, k, i)):
+                yield [l, r]
+
+# read rules
 for line in open (sys.argv[1]):
     rule = line[:-1].split (" ")
     lhs  = rule[0]
@@ -47,14 +62,17 @@ for line in iter (sys.stdin.readline, ""): # no buffering
         sentence  = line[:-1].split (" ", 1)[1][1:-1].split (" ")
         cky_table = cky_parse (sentence)
         print "success" if 'S' in cky_table[0][len (sentence)] else 'fail'
-    else:
+    elif command == 'stat':
         if sentence:
-            if command == 'show_stat':
-                print "sentence:  %s"      % ' '.join (sentence)
-                print "length:    %d"      % len (sentence)
-                print "# edges:   %d"      % num_edges (cky_table)
-#            print "# parse trees: %d\n" % num_parse_trees ())
-            elif command == 'show_trees':
-#                print_trees ()
-                pass
+            print "sentence: %s"     % ' '.join (sentence)
+            print "length:   %d"     % len (sentence)
+            print "# edges:  %d"     % num_edges (cky_table)
+            print "# trees:  %d" % num_trees (cky_table, 'S', 0, len (sentence))
+    elif command == 'print':
+        if sentence:
+            for tree in recover_trees (cky_table, 'S', 0, len (sentence)):
+                # print_tree (revert_rule (tree))
+                print tree
+    else:
+        sys.stderr.write ("unknown command: %s\n" % command)
     sys.stdout.write ("> ")
