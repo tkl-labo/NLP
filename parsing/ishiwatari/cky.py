e4162103f8263911203ffe44481cff26cd2c106d
S. Ishiwatari <ishiwatari@tkl.iis.u-tokyo.ac.jp>
2014-12-10 11:31:00 +0900

    clean up cky.py

--- parsing/ishiwatari/cky.py
+++ parsing/ishiwatari/cky.py
@@ -19,29 +19,6 @@ def read_rules(filename):
 	return grammar, lexicon
 
 
-def cky_recognition_old(s, grammar, lexicon):
-	cky = [[[] for j in range(len(s) + 1)] for i in range(len(s))]
-	
-	for j in range(len(s) + 1):
-		for i in reversed(range(j)):
-
-			if j - i == 1: # グラフの底辺。lexiconを参照する場合
-				for key in lexicon.keys():
-					if s[i] in lexicon[key]:
-						cky[i][j].append({'head': key})
-						
-			else: # グラフの底辺以外。各cky[i][k]、cky[j][k]の組み合わせを右辺に持つようなルールをcky[i][j]に追加する
-				for k in range(i + 1, j):
-					for left in cky[i][k]:
-						for right in cky[k][j]:
-							for candidate in grammar.keys():
-								if exists_in_grammar(candidate, left['head'], right['head'], grammar):
-										cky[i][j].append({'head': candidate, 'left': left['head'], 'left_place':(i, k), 'right': right['head'], 'right_place':(k, j)})
-			print 'cky[', i, '][', j, ']:', cky[i][j]
-			
-	return cky
-
-
 def cky_recognition(s, grammar, lexicon):
 	cky = [[[] for j in range(len(s) + 1)] for i in range(len(s))]
 	
