05db07a11ed105670df5624b731fb2399a98f174
Naoki Yoshinaga <y.naoki@gmail.com>
2014-12-10 18:18:17 +0900

    rewrite to support unit productions

--- parsing/yng/to_cnf.py
+++ parsing/yng/to_cnf.py
@@ -3,55 +3,62 @@
 #            (assuming terminals are prefixed by _)
 import sys, collections
 
-# read rules and convert them to CNF
-i = 1
-solo_rules      = {}
-unit_rules      = []
-unit_rules_prob = {}
-l2rs = collections.defaultdict (lambda: collections.defaultdict (float))
+if len (sys.argv) >= 3:
+    sys.exit ("Usage: %s noconv_unit" % sys.argv[0])
+
+grammar     = []
+new_grammar = collections.defaultdict (float)
+l2rs        = collections.defaultdict (list)
+
+# read rules
 for line in sys.stdin:
-    rule           = line[:-1].split (" ")
+    rule = line[:-1].split (" ")
     lhs, rhs, prob = rule[0], rule[1:-1], float (rule[-1])
-    if len (rhs) == 1:
-        if rhs[0][0] != '_': # handle unit production later
-            unit_rules.append (tuple ([lhs, tuple (rhs)]))
-            unit_rules_prob[tuple ([lhs, tuple (rhs)])] = [prob, [lhs, rhs[0]]]
-    else:
-        # replace all terminals with non-terminals
+    l2rs[lhs].append (len (grammar))
+    grammar.append ([lhs, rhs, prob])
+
+# convert terminals within non-lexical rules to dummy non-terminals
+for lhs, rhs, prob in grammar:
+    if len (rhs) >= 2:
         for j in range (0, len (rhs)):
             if rhs[j][0] == '_':
                 lhs_ = 'Z' + rhs[j]
-                l2rs[lhs_][tuple (rhs[j:j+1])] = 1.0
-                rhs[i] = lhs_
-        # generate a rule to replace first two non-terminals to one
-        while len (rhs) > 2:
-            if tuple (rhs[:2]) not in solo_rules:
-                solo_rules[tuple (rhs[:2])] = 'X' + str (i)
-                i += 1
+                new_grammar[(lhs_, tuple (rhs[j:j + 1]))] = 1.0
+                rhs[j] = lhs_
+
+# convert unit productions; assuming no loop
+replaced = set ()
+if len (sys.argv) == 1:
+    j = 0
+    while j < len (grammar):
+        lhs, rhs, prob = grammar[j]
+        if len (rhs) == 1 and rhs[0][0] != '_':
+            replaced.add (j)
+            for k in l2rs[rhs[0]]:
+                if k not in replaced:
+                    _, rhs_, prob_ = grammar[k]
+                    l2rs[lhs].append (len (grammar))
+                    grammar.append ([lhs, rhs_, prob * prob_])
+        j += 1
+
+# binarize all rules and add to new grammar.
+solo_rules = {}
+i = 1
+for j in range (len (grammar)):
+    if j in replaced:
+        continue
+    lhs, rhs, prob = grammar[j]
+    while len (rhs) >= 3:
+        if tuple (rhs[:2]) not in solo_rules:
+            lhs_ = 'X' + str (i)
+            solo_rules[tuple (rhs[:2])] = lhs_
+            new_grammar[(lhs_, tuple (rhs[:2]))] = 1.0
+            i += 1
+        else:
             lhs_ = solo_rules[tuple (rhs[:2])]
-            l2rs[lhs_][tuple (rhs[:2])] = 1.0
-            rhs[:2] = [lhs_]
-    l2rs[lhs][tuple (rhs)] = prob
+        rhs[:2] = [lhs_]
+    new_grammar[(lhs, tuple (rhs))] += prob
 
-# handle unit productions
-while unit_rules:
-    rule = unit_rules.pop ()
-    (lhs, rhs), (prob, loop) = rule, unit_rules_prob[rule]
-    unit_rules_prob.pop (rule)
-    l2rs[lhs].pop (rhs)
-    for rs, prob_ in l2rs[rhs[0]].items ():
-        if len (rs) == 1 and rs[0][0] != '_': # yet unit production
-            if rs[0] in loop:
-                sys.stderr.write ("loop found: %s\n" % (' -> '.join (loop + rs)))
-                sys.exit ()
-            else:
-                if tuple ([lhs, rs]) not in unit_rules_prob:
-                    unit_rules.append (tuple ([lhs, rs]))
-                    unit_rules_prob[tuple ([lhs, rs])] = [0, loop]
-                unit_rules_prob[tuple ([lhs, rs])][0] += prob * prob_
-                unit_rules_prob[tuple ([lhs, rs])][1].append (rs[0])
-        l2rs[lhs][rs] += prob * prob_
-
-for lhs in l2rs.keys ():
-    for rhs, prob in l2rs[lhs].items ():
-        print lhs, ' '.join (rhs), prob
+# output new_grammar
+for (lhs, rhs), prob in new_grammar.items ():
+    print lhs, ' '.join (rhs), prob
