d20f1dc1cea58e665dc08aec0a061ecc16d8db1d
Shoetsu Sato <shoetsu@tkl.iis.u-tokyo.ac.jp>
2015-06-18 03:23:15 +0900

    convertion between wstr and str

--- edit_distance/shoetsu/src/util.cpp
+++ edit_distance/shoetsu/src/util.cpp
@@ -2,12 +2,44 @@
 #include "util.h"
 
 #include <sys/time.h>
+#include <wchar.h>
+
 using namespace std;
+
+//=========================
+//   Measuring
+//=========================
 
 double cur_time(){
   struct timeval tp[1];
   gettimeofday(tp,0);
   return tp->tv_sec + 1.0e-6 * tp->tv_usec;
+}
+
+
+
+//============================
+//   Multi-Byte String
+//===========================
+
+
+
+wstring widen(const std::string &src) 
+{
+	wchar_t *wcs = new wchar_t[src.length() + 1];
+	mbstowcs(wcs, src.c_str(), src.length() + 1);
+	wstring dest = wcs;
+	delete [] wcs;
+	return dest;
+}
+
+string narrow(const std::wstring &src) 
+{
+	char *mbs = new char[src.length() * MB_CUR_MAX + 1];
+	wcstombs(mbs, src.c_str(), src.length() * MB_CUR_MAX + 1);
+	string dest = mbs;
+	delete [] mbs;
+	return dest;
 }
 
 
--- edit_distance/shoetsu/src/util.h
+++ edit_distance/shoetsu/src/util.h
@@ -3,9 +3,11 @@
 #ifndef UTIL_H_INCLUDED
 #define UTIL_H_INCLUDED
 
-
 #include <string>
 
 double cur_time();
+
+std::wstring widen(const std::string &);
+std::string narrow(const std::wstring &src);
 
 #endif
