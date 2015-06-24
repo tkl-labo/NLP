3bff2fba060cd67e0c7531101ad963720b02ce96
tiwanari <tatsuyaw0c@gmail.com>
2015-06-25 03:37:08 +0900

    implemented counter

--- n_gram/iwanari/src/main.cc
+++ n_gram/iwanari/src/main.cc
@@ -4,14 +4,24 @@
 #include "stopwatch.h"
 #include "n_gram.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] 
+            << " training_data [N = 2]" << std::endl;
+        return 0;
+    }
+    const int N = (argc >= 3) ? std::atoi(argv[2]) : 2;
+    
     setlocale(LC_ALL, "ja_JP.UTF-8");
-
+    
     util::Stopwatch<std::chrono::milliseconds> sw;
-    nlp::NGram ngram("../hoge.txt");
-    ngram.train(1);
+    nlp::NGram ngram(argv[1]);
+    ngram.train(N);
     sw.stop();
+    
+    ngram.showAllProbabilities();
+    
     std::cout << "Elapsed: " << sw.showElapsedTime().c_str() << std::endl;
 
     return 0;
--- n_gram/iwanari/src/n_gram.h
+++ n_gram/iwanari/src/n_gram.h
@@ -1,26 +1,42 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <map>
+#include <memory>
 
 namespace nlp
 {
+
+typedef std::vector<std::string> NGramKey;
+
 class NGram
 {
 private:
+    struct Node {
+        int m_total_freq;
+        std::unordered_map<std::string, int> m_freqs;
+        Node() : m_total_freq(0) {}
+        bool insertKey(const std::string& key);
+    };
+
+    int m_num_of_ngrams;
+    int m_N;
     std::string m_training;
-    int m_num_of_pairs;
-    
+    std::map<NGramKey, NGram::Node> m_root;
+
 public:
     NGram(const std::string training) 
-    : m_num_of_pairs(0) {
-        m_training = training;
-    }
+        : m_training(training) { }
     virtual ~NGram() {}
     
     std::string trainingFileName() { return m_training; }
-    void train(const int n);
+    void train(const int N);
+    void showAllProbabilities();
+    void showAllNgrams();
 
 private:
     void readFile();
+    void constructTree(std::istream &stream);
 };
 }
