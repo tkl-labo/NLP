d68894bba8901bd25597c53a843369cc345a5930
Shoetsu Sato <shoetsu@tkl.iis.u-tokyo.ac.jp>
2015-06-25 04:00:27 +0900

    update

--- n_gram/shoetsu/run.sh
+++ n_gram/shoetsu/run.sh
@@ -1,11 +1,25 @@
-
-CORPUS="data/2012-01-01.cdr.gz"
+METHOD="load"
 NLINE=500000
+N=3
+CORPUS="data/2012-01-01.cdr.gz"
+
 if [ $# -gt 0 ]; then
-  NLINE=$1
+  METHOD=$1
 fi
 if [ $# -gt 1 ]; then
-  CORPUS=$2
+  N=$2
 fi
- 
-gzip -dc $CORPUS | head -$NLINE | LC_ALL=C cut -f 1 | ./a.out 2
+
+if [ $METHOD = "learn" ]; then
+   if [ $# -gt 2 ]; then
+       NLINE=$3
+   fi
+   if [ $# -gt 3 ]; then
+       CORPUS=$4
+   fi
+   gzip -dc $CORPUS | head -$NLINE | LC_ALL=C cut -f 1 | ./a.out $METHOD $N
+else
+   ./a.out $METHOD $N
+fi
+
+#gzip -dc $CORPUS |  LC_ALL=C cut -f 1 | ./a.out 2
