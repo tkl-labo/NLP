8dc6503b13e1ca66c5a6b5d7d81f8249fa63951f
Naoki Yoshinaga <y.naoki@gmail.com>
2015-06-25 07:54:28 +0900

    bug fix

--- n_gram/yng/perplexity.cc
+++ n_gram/yng/perplexity.cc
@@ -2,47 +2,53 @@
 #include <cstdlib>
 #include <cmath>
 #include <vector>
+#include <tr1/unordered_set>
 #include "cedarpp.h"
-
-static const int V = 5; //632237;
-static const int k = 5;
 
 int main (int argc, char** argv) {
   if (argc < 3)
     { std::fprintf (stderr, "Usage: %s\n", argv[0]); std::exit (1); }
-  enum smoothing_t { NONE = 0, LAPLACE = 1, GOOD_TURING = 2 };
+  enum smoothing_t { NONE = 0, LAPLACE = 1 };
   
   typedef cedar::da <int, -1, -2, true>  trie_t;
   trie_t trie;
+  trie_t vocabulary;
   
   char line[1024];
   FILE* fp = std::fopen (argv[1], "r");
   size_t N = 0;
   size_t C = 0;
-//  std::vector <trie_t> tries (k + 1);
+  size_t V = 0;
   while (std::fgets (line, 1 << 21, fp) != NULL) {
     char* p = 0;
     int count = static_cast <int> (std::strtol (line, &p, 10));
-    char* q = ++p;
     char* r = p;
+    char* q = ++p;
     do {
       while (*q != ' ' && *q != '\n') ++q;
       if (*q == ' ') r = q;
     } while (*q++ != '\n');
     *--q = '\0';
+    // check N in N-grams
     if (! N) { for (char* s = p; *s; ++s) { if (*s == ' ') ++N; }; ++N; }
+    // register an entire N-gram
     trie.update (p, q - p)  = count;
+    // register N-1-gram (denominator)
     if (N == 1)
       C += count;
     else
       trie.update (p, r - p) += count;
+    // register a (last) word to vocabulary
+    ++r;
+    int &id = vocabulary.update (r, q - r);
+    if (! id) id = ++V;
   }
-  std::fprintf (stderr, "# %ld-grams: %ld; # vocabulary: %d\n", N, trie.num_keys (), V);
+  std::fprintf (stderr, "# %ld-grams: %ld; # vocabulary: %ld\n", N, trie.num_keys (), V);
   //
   smoothing_t type = static_cast <smoothing_t> (std::strtol (argv[2], NULL, 10));
   //
   std::vector <const char*> words;
-  for (size_t i = 0; i < N; ++i)
+  for (size_t i = 0; i < N - 1; ++i)
     words.push_back ("<s>");
   size_t sum  = 0;
   double logP = 0;
@@ -54,7 +60,7 @@ int main (int argc, char** argv) {
     }
     words.push_back ("</s>");
     sum += words.size () - (N - 1);
-    for (size_t i = 0; i <= words.size () - N; ++i) {
+    for (size_t i = N - 1; i < words.size (); ++i) {
       cedar::npos_t from = 0;
       size_t pos = 0;
       int n (0), m (0);
@@ -62,18 +68,20 @@ int main (int argc, char** argv) {
         n = C;
         m = trie.traverse (words[i], from, pos = 0);
       } else {
-        size_t j = i;
+        size_t j = i - (N - 1);
         bool found = true;
-        for (; j < i + N - 1; ++j)
+        // count for N-1-gram (traversing one word by one)
+        for (; j < i; ++j)
           if ((n = trie.traverse (words[j], from, pos = 0)) == -2 ||
-              (j != i + N - 2 &&
+              (j != i - 1 &&
                trie.traverse (" ", from, pos = 0, 1) == -2)) {
             found = false;
             break;
           }
+        // count for N-gram
         if (found && n >= 0)
           if (trie.traverse (" ", from, pos = 0, 1) != -2)
-            m = trie.traverse (words[i + N - 1], from, pos = 0);
+            m = trie.traverse (words[i], from, pos = 0);
       }
       m = std::max (m, 0);
       n = std::max (n, 0);
@@ -81,17 +89,14 @@ int main (int argc, char** argv) {
       assert (m != 0);
       assert (n != 0);
       switch (type) {
-        case NONE:    P = m / n; break;
+        case NONE:    P = m * 1.0 / n; break;
         case LAPLACE: P = (m + 1.0) / (n + V + 1.0); break;
-//        case GOOD_TURING:
-//          if (m == 0)
-//            m = nc[1] / (std::pow (V, N));
-//          else if (m <= k)
-//            m = ((m + 1) * nc[m + 1] / nc[m] - c * (k + 1) * nc[k + 1] / nc[1]) / (1 - (k + 1) * nc[k + 1] / k);
       }
+      std::fprintf (stdout, "%s\t%f\n", words[i], - std::log (P));
       logP += std::log (P);
     }
+    std::fprintf (stdout, "\n");
   }
-  std::fprintf (stderr, "perplexity: %f\n", std::exp (logP * -1.0 / sum));
+  std::fprintf (stderr, "perplexity: %f\n", std::exp (- logP / sum));
   return 0;
 }
