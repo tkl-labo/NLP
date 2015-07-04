63d2f1049aafac01d7fe0b0ad07d24cc21e9e9af
tiwanari <tatsuyaw0c@gmail.com>
2015-07-05 00:39:02 +0900

    added placeholder

--- hmm/iwanari/tagger.cc
+++ hmm/iwanari/tagger.cc
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include "tagger.h"
+using namespace nlp;
+
+void Tagger::train(const std::string &training)
+{
+    std::cout << "training..." << std::endl;
+    std::ifstream input_file(training);
+    if (!input_file) {
+        std::cerr << "ERROR: No such file (" 
+                    << training << ")" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    input_file.close();
+    std::cout << "trained!" << std::endl;
+}
+
+void Tagger::test(const std::string &testing)
+{
+}
