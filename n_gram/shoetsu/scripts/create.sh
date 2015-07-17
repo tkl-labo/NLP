62205b1012337796a8279148e2619e8de7b63ed4
Shoetsu Sato <shoetsu@tkl.iis.u-tokyo.ac.jp>
2015-07-17 20:11:33 +0900

    update

--- n_gram/shoetsu/scripts/create.sh
+++ n_gram/shoetsu/scripts/create.sh
@@ -1,20 +1,29 @@
 METHOD="create"
 N=3
+KEYWORDS=""
 
 if [ $# -lt 1 ]; then
   echo "./create.sh (2|3) [loadfile]"
-  exit 1 
+  exit 1
 fi
 
 N=$1
 
+
+
+
 if [ $# -gt 1 ]; then
     FILE=$2
+    if [ $# -gt 2 ]; then
+	KEYWORDS=$3
+    fi
 else
     if [ $N -eq 2 ]; then
-	FILE="data/bi_gram.dat"
+	FILE="trained/bi_gram.dat"
     elif [ $N -eq 3 ]; then
-	FILE="data/tri_gram.dat"
+	FILE="trained/tri_gram.dat"
     fi
 fi
-./a.out $METHOD $N $FILE
+
+./a.out $METHOD $N $FILE $KEYWORDS
+    
