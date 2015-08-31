85c1c000e78e553a87288f3aad724bf64c1aa2ba
tiwanari <tatsuyaw0c@gmail.com>
2015-09-01 07:33:09 +0900

    fin cky base

--- parsing/iwanari/cfg_to_cnf.rb
+++ parsing/iwanari/cfg_to_cnf.rb
@@ -1,52 +1,10 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 require "./colored"
+require "./nlp_util"
 
 is_debug = true
 is_debug = false
-
-class String
-    def terminal?
-        self[0] == "_"
-    end
-
-    def nonterminal?
-        !(self[0] == "_")
-    end
-end
-
-def normalized?(rule)
-    lhs, rhs, prob = rule
-    # 3 normalized pattern
-    if (rhs.length == 2 and rhs[0].nonterminal? and rhs[1].nonterminal?) or 
-        (rhs.length == 1 and rhs[0].terminal?) or 
-        (rhs.length == 0 and lhs == "S") then
-        true
-    else
-        false
-    end
-end
-
-def show_and_normalized_check(rules)
-    is_normalized = true
-    for rule in rules do
-        if normalized?(rule)
-            print rule
-        else
-            print rule.to_s.pink
-            is_normalized = false
-        end
-        puts ""
-    end
-    
-    # show check result
-    if is_normalized then
-        puts "Normalized!".green
-    else
-        puts "Not Normalized...".red
-    end
-end
-
 
 # ===== Initialize =====
 # check input argument
