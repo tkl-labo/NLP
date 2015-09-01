7901235be720347488f6f3c866e866e720f34afa
Mika Koizumi <o.o.8.o.o.bell@gmail.com>
2015-09-01 15:42:39 +0900

    introduced PCKY

--- parsing/koizumi/parse.rb
+++ parsing/koizumi/parse.rb
@@ -7,34 +7,36 @@ class Parse
     include_rules(rules_file)
   end
 
-  def cky_parse sentence
+  def cky_parse sentence, pflag: false
     input = sentence.split(" ")#(/\s+|\.|,|/)
     length = input.size
+    @pflag=pflag
     @memo = Array.new(length).map{Array.new(length+1).map{Array.new}}
-    @prob= Array.new(length).map{Array.new(length+1).map{Hash.new}}
     #POSタグを終端記号として入れる
     input.each_with_index do |word, ind|
       raise word + " doesn't exist in lexicon" if (rules = @lexicon.select {|item| item[1][1..-1] == word}).empty?
-      rules.each {|rule| @memo[ind][ind+1].push({left: rule[0], right: [rule[1]]})}
-      @memo[ind][ind+1].each {|sym| merge_bp_array(left_hand_of(sym), @memo[ind][ind+1])} #loopがあっても大丈夫
+      rules.each do |rule| 
+        @memo[ind][ind+1].push({left: rule[0], right: [rule[1]], prob:rule[2].to_f, count: 1})
+      end
+      @memo[ind][ind+1].each {|sym| merge_bp_array(left_hand_of(sym), ind, ind+1)} #loopがあっても大丈夫
     end
     1.upto(length) do |i|
       (i-2).downto(0) do |j|
         (j+1).upto(i-1) do |k|
           @memo[j][k].each do |sym1|
             @memo[k][i].each do |sym2|
-              merge_bp_array(left_hand_of(sym1, symptr2: sym2), @memo[j][i])
+              merge_bp_array(left_hand_of(sym1, symptr2: sym2), j, i)
             end
           end
         end
-        @memo[j][i].each {|sym| merge_bp_array(left_hand_of(sym), @memo[j][i])}
+        @memo[j][i].each {|sym| merge_bp_array(left_hand_of(sym), j,i)}
       end
     end
     #debug length
-    #@memo[0][length][2][:right].each {|i| p i}
     display length
   end
-  def cky_prob_parse
+  def cky_prob_parse sentence
+    cky_parse sentence, pflag: true
   end
   def early_parse
   end
@@ -57,31 +59,40 @@ class Parse
     unless symptr2
       @rules_1.each do |rule|
         if rule[1] == symptr1[:left]
-          symbols.push({left: rule[0], right: [symptr1]})
+          symbols.push({left: rule[0], right: [symptr1], prob: rule[2].to_f*symptr1[:prob], count: symptr1[:count]})
         end
       end
     else
       @rules_2.each do |rule|
-        symbols.push({left: rule[0], right: [symptr1, symptr2]}) if rule[1] == symptr1[:left] && rule[2] == symptr2[:left]
+        symbols.push({left: rule[0], right: [symptr1, symptr2], prob: rule[3].to_f*symptr1[:prob]*symptr2[:prob], count: symptr1[:count]*symptr2[:count]}) if rule[1] == symptr1[:left] && rule[2] == symptr2[:left]
       end
     end
     symbols
   end
-  def merge_bp_array new_ary, updated
+  def merge_bp_array new_ary, i, j 
     new_ary.each do |new|
-      if (same_left_bp = updated.find {|upd| new[:left] == upd[:left]})
-        same_left_bp[:right] = [same_left_bp[:right], new[:right]]
+      if (same_left_bp = @memo[i][j].find {|upd| new[:left] == upd[:left]})
+        if @pflag
+          if same_left_bp[:prob] < new[:prob]
+            same_left_bp[:right] = new[:right]
+            same_left_bp[:prob] = new[:prob]
+          end
+        else
+          same_left_bp[:right] = [same_left_bp[:right], new[:right]]
+        end
+        same_left_bp[:count] += new[:count]
         new_ary.delete(new)
       end
     end
-    updated.concat(new_ary)
+    @memo[i][j].concat(new_ary)
   end
   def display length
     unless @memo[0][length].empty?
       puts "success"
       puts "edges: "
-      puts "trees: "+ @memo[0][length].count.to_s
+      puts "trees: "+ @memo[0][length].first[:count].to_s
       @memo[0][length].select! {|tree| tree[:left] == "S"}
+      puts @memo[0][length].first[:prob] if @pflag
       display_tree  @memo[0][length], 0
     else
       puts "failed"
